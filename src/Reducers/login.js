4f54665b8ff7aa155f7b867869042f28d83da2aa
Bernardo Orozco Garza <berny.orozco@hotmail.com>
2018-09-25 17:19:08 -0500

    dommy login

--- src/Reducers/login.js
+++ src/Reducers/login.js
@@ -2,12 +2,14 @@ export default (
   state = {
     email: "",
     password: "",
+    error: false,
+    token: null
   },
   action
 ) => {
   let newState = { ...state };
   switch (action.type) {
-    case "SET_VALUE":
+    case "SET_LOGIN_VALUE":
       newState[action.key] = action.value;
       break;
   }
