322f8d60e8c15ef0736ec84f63219ab4283d9abc
Bruno Mendez <bruno.m_@hotmail.com>
2018-09-25 17:36:59 -0500

    added backend.js

--- src/backend.js
+++ src/backend.js
@@ -1,5 +1,76 @@
 import { CarbonLDP } from "carbonldp";
+import { AccessPoint } from "carbonldp/AccessPoint";
 
-export function hello() {
-  console.log("hello");
-}
+import "./styles.css";
+
+console.log(CarbonLDP.version);
+
+const carbonldp = new CarbonLDP("https://db.itesm-02.carbonldp.com/");
+carbonldp.extendObjectSchema("Persona", {
+    "nombre": {
+        "@type": "string"
+    },
+    "birthDate": {
+        "@type": "date"
+    },
+    "padres": {
+        "@type": "@id",
+        "@container": "@set"
+    },
+    "hijos": {
+        "@type": "@id",
+        "@container": "@set"
+    },
+    "empresa": {
+        "@type": "@id"
+    },
+    "profesion": {
+        "@type": "string"
+    },
+    "hobby": {
+        "@type": "@id",
+        "@container": "@set"
+
+    }
+});
+carbonldp.extendObjectSchema("Hobby", {
+    "nombre": {
+        "@type": "string"
+    },
+    "eventos": {
+        "@type": "@id",
+        "@container": "@set"
+    },
+    "materiales": {
+        "@type": "string",
+        "@container": "@set"
+    },
+    "tags": {
+        "@type": "@id",
+        "@container": "@set"
+    }
+});
+carbonldp.extendObjectSchema("Tags", {
+    "name": {
+        "@type": "string"
+    },
+    "subTags": {
+        "@type": "@id",
+        "@container": "@set"
+    }
+});
+carbonldp.extendObjectSchema("Empresa",{
+    "name": {
+        "@type": "string"
+    },
+    "location": {
+        "@type": "string"
+    }, 
+    "dateCreated": {
+        "@type": "date"
+    },
+    "employees": {
+        "@type": "@id",
+        "@container": "@set"
+    }
+});
