0c167e4bd9bf42d031562d91169e8478d758c0b1
Bernardo Orozco Garza <berny.orozco@hotmail.com>
2018-09-26 14:47:27 -0500

    Account template

--- src/Components/Account/index.js
+++ src/Components/Account/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Container } from "semantic-ui-react";
+import { Container, Grid, Image, Menu, Header } from "semantic-ui-react";
 
 class Account extends Component {
   constructor(props) {
@@ -11,7 +11,39 @@ class Account extends Component {
   render() {
     return (
       <Container>
-        <p>account</p>
+        <Header as="h1">Account</Header>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <Menu pointing secondary vertical>
+                <Menu.Item
+                  name="home"
+                  active={false}
+                  onClick={() => {
+                    alert("hello");
+                  }}
+                />
+                <Menu.Item
+                  name="messages"
+                  active={true}
+                  onClick={() => {
+                    alert("hello");
+                  }}
+                />
+                <Menu.Item
+                  name="friends"
+                  active={false}
+                  onClick={() => {
+                    alert("hello");
+                  }}
+                />
+              </Menu>
+            </Grid.Column>
+            <Grid.Column width={13}>
+              <Image src="https://react.semantic-ui.com/images/wireframe/centered-paragraph.png" />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </Container>
     );
   }
