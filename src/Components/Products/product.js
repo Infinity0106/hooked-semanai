c9d55c198af1f05bcf43e3c2c795872d57ed4a86
Bruno Mendez <bruno.m_@hotmail.com>
2018-09-26 17:13:01 -0500

    added images and tags

--- src/Components/Products/product.js
+++ src/Components/Products/product.js
@@ -14,16 +14,16 @@ class Product extends Component {
       <Grid.Column>
         <Card>
           <Image
-            src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
+            src={this.props.image}
             label={Ctrl.renderWishlist.bind(this, this.props.logged_in)}
           />
           <Card.Content>
-            <Card.Header>Matthew</Card.Header>
+            <Card.Header>{this.props.name}</Card.Header>
             <Card.Meta>
-              <span className="date">Joined in 2015</span>
+              <span className="date">{this.props.tags}</span>
             </Card.Meta>
             <Card.Description>
-              Matthew is a musician living in Nashville.
+              {this.props.desc}
             </Card.Description>
           </Card.Content>
           <Card.Content
