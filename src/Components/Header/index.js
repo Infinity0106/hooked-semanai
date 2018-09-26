915f0c19ef6f07784ec2c6878972af4ff5f097a8
Bernardo Orozco Garza <berny.orozco@hotmail.com>
2018-09-26 14:48:31 -0500

    Add Header dropdown

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Menu, Segment, Container, Dropdown, Image } from "semantic-ui-react";
+import {
+  Menu,
+  Segment,
+  Container,
+  Dropdown,
+  Image,
+  Icon,
+  Button
+} from "semantic-ui-react";
 import * as Ctrl from "./ctrl";
 import { withRouter } from "react-router-dom";
+import Link from "react-router-dom/Link";
 
 class Header extends Component {
   constructor(props) {
@@ -12,51 +21,74 @@ class Header extends Component {
   componentWillMount() {}
   render() {
     return (
-      <Segment inverted attached={"top"}>
+      <Segment inverted attached basic borderless>
         <Menu inverted pointing secondary>
           <Container>
             <Menu.Item
-              name="home"
+              name="Home"
               data_path="/"
               data_key="landing"
               active={this.props.active === "landing"}
               onClick={Ctrl.menuClick.bind(this)}
             />
             <Menu.Item
-              name="messages"
-              active={this.props.active === "landing"}
+              name="Eventos"
+              data_path="/eventos"
+              data_key="event"
+              active={this.props.active === "event"}
               onClick={Ctrl.menuClick.bind(this)}
             />
             <Menu.Item
-              name="friends"
-              active={this.props.active === "landing"}
+              name="Productos"
+              data_path="/products"
+              data_key="products"
+              active={this.props.active === "products"}
               onClick={Ctrl.menuClick.bind(this)}
             />
             <Menu.Menu position="right">
-              <Dropdown
-                item
-                simple
-                style={{ padding: 5 }}
-                trigger={
-                  <span>
-                    <Image
-                      avatar
-                      src={
-                        "https://s3.amazonaws.com/uifaces/faces/twitter/kuldarkalvik/128.jpg"
-                      }
-                    />{" "}
-                    {"juancho perez"}
-                  </span>
-                }
-              >
-                <Dropdown.Menu>
-                  <Dropdown.Item>Mi perfil</Dropdown.Item>
-                  <Dropdown.Divider />
-                  <Dropdown.Item as="a" onClick={Ctrl.logOut.bind(this)}>
-                    Log-out
-                  </Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
+              {!this.props.logged_in && (
+                <Button
+                  basic
+                  inverted
+                  icon
+                  labelPosition="left"
+                  onClick={Ctrl.logIn.bind(this)}
+                >
+                  <Icon name="user circle outline" />
+                  Login
+                </Button>
+              )}
+              {this.props.logged_in && (
+                <Dropdown
+                  item
+                  simple
+                  style={{ padding: 5 }}
+                  trigger={
+                    <span>
+                      <Image
+                        avatar
+                        src={
+                          "https://s3.amazonaws.com/uifaces/faces/twitter/kuldarkalvik/128.jpg"
+                        }
+                      />{" "}
+                      {"juancho perez"}
+                    </span>
+                  }
+                >
+                  <Dropdown.Menu>
+                    <Dropdown.Item
+                      as="a"
+                      onClick={Ctrl.accountClick.bind(this)}
+                    >
+                      Mi perfil
+                    </Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item as="a" onClick={Ctrl.logOut.bind(this)}>
+                      Log-out
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              )}
             </Menu.Menu>
           </Container>
         </Menu>
@@ -71,7 +103,8 @@ export default withRouter(
   connect(store => {
     return {
       // data: store.nameElementStore
-      active: store.header.active
+      active: store.header.active,
+      logged_in: store.login.token != null
     };
   })(Header)
 );
