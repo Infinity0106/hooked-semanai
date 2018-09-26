ca90c372f42275df6075d65ef380a19c8910f8e8
Bernardo Orozco Garza <berny.orozco@hotmail.com>
2018-09-26 12:24:39 -0500

    Template events

--- src/Components/Events/index.js
+++ src/Components/Events/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Container } from "semantic-ui-react";
+import { Container, Header, Grid, Image } from "semantic-ui-react";
 
 class Events extends Component {
   constructor(props) {
@@ -11,7 +11,17 @@ class Events extends Component {
   render() {
     return (
       <Container>
-        <p>events</p>
+        <Header as="h1">Eventos</Header>
+        <Grid columns={2} divided>
+          <Grid.Row>
+            <Grid.Column>
+              <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+            </Grid.Column>
+            <Grid.Column>
+              <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </Container>
     );
   }
