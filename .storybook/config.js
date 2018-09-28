4f352ca0e5837d209c7ac7d5d84e49e80fbc4b8a
Bernardo Orozco Garza <berny.orozco@hotmail.com>
2018-09-28 11:27:54 -0500

    Final config stooryboard

--- .storybook/config.js
+++ .storybook/config.js
@@ -6,10 +6,10 @@ function loadStories() {
   require("../stories/Events/index.js");
   require("../stories/Header/index.js");
   require("../stories/Home/index.js");
-  // require("../stories/Landing/index.js");
-  // require("../stories/Login/index.js");
+  require("../stories/Landing/index.js");
+  require("../stories/Login/index.js");
   require("../stories/NotFound/index.js");
-  // require("../stories/Products/index.js");
+  require("../stories/Products/index.js");
   require("../stories/ShoppingBag/index.js");
   require("../stories/WishList/index.js");
 }
