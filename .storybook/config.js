7fad8351459768480b098c8ba79c11e19178cc09
Bernardo Orozco Garza <berny.orozco@hotmail.com>
2018-09-28 10:22:15 -0500

    Add general documentation

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,7 +3,7 @@ import { configure } from "@storybook/react";
 function loadStories() {
   require("../stories/Account/index.js");
   require("../stories/Error/index.js");
-  // require("../stories/Events/index.js");
+  require("../stories/Events/index.js");
   // require("../stories/Header/index.js");
   // require("../stories/Home/index.js");
   // require("../stories/Landing/index.js");
